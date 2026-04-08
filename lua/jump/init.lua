4dee4c606c9f45d0422a1f28590bd897ffc4f757
Hee ZiXi <zixi.hee@paynet.my>
2026-04-08 17:31:25 +0800

    change priority

--- lua/jump/init.lua
+++ lua/jump/init.lua
@@ -124,7 +124,7 @@ function M.start()
           hl_group = CONFIG.backdrop,
           end_row = line,
           hl_eol = true,
-          priority = 0,
+          priority = 5000,
           strict = false,
         })
       end
@@ -150,7 +150,7 @@ function M.start()
           CONFIG.search,
           { match.line, match.start_col },
           { match.line, match.end_col },
-          { priority = 200 }
+          { priority = 5001 }
         )
 
         if label then
@@ -158,7 +158,7 @@ function M.start()
           api.nvim_buf_set_extmark(buf, NS, match.line, match.start_col, {
             virt_text = { { label, CONFIG.label } },
             virt_text_pos = 'overlay',
-            priority = 201,
+            priority = 5002,
           })
         end
       end
